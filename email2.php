6b8ec5b61fcbba2aab2b46f8fc2864a585241424
Rajesh <rjpethkar@gmail.com>
2020-01-30 13:20:30 +0530

    updates

---
+++ email2.php
@@ -0,0 +1,55 @@
+<?php
+$subject="Test email from Yeshiva PHP";
+
+$subject = $_REQUEST['msg_message'];
+$to="rajpethkar@gmail.com";
+$message="06-Oct-19 04:40";
+$headers="From: rajesh.pethkar@yeshiva-systems.com";
+echo $subject;
+
+$filename = $_FILES["photo"]["name"];
+echo $filename;
+
+if (mail($to,$subject,$message,$headers)) {
+echo "Mail send successfully";
+} else {
+echo "Cannot sent email";
+}
+
+// Check if the form was submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    // Check if file was uploaded without errors
+    if(isset($_FILES["photo"]) && $_FILES["photo"]["error"] == 0){
+        $allowed = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+        $filename = $_FILES["photo"]["name"];
+        $filetype = $_FILES["photo"]["type"];
+        $filesize = $_FILES["photo"]["size"];
+    
+        // Verify file extension
+        $ext = pathinfo($filename, PATHINFO_EXTENSION);
+        if(!array_key_exists($ext, $allowed)) die("Error: Please select a valid file format.");
+    
+        // Verify file size - 5MB maximum
+        $maxsize = 5 * 1024 * 1024;
+        if($filesize > $maxsize) die("Error: File size is larger than the allowed limit.");
+    
+        // Verify MYME type of the file
+        if(in_array($filetype, $allowed)){
+            // Check whether file exists before uploading it
+            if(file_exists("upload/" . $filename)){
+                echo $filename . " is already exists.";
+            } else{
+                move_uploaded_file($_FILES["photo"]["tmp_name"], "upload/" . $filename);
+                echo "Your file was uploaded successfully.";
+				header( "refresh:2;url=ticket.html" );
+				exit;
+            } 
+        } else{
+            echo "Error: There was a problem uploading your file. Please try again."; 
+        }
+    } else{
+        echo "Error: " . $_FILES["photo"]["error"];
+    }
+}
+
+?>
